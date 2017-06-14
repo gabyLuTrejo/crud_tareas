b0e6a971a2df126b7a9b30f5b2062b9490be1bd6
gabytrejo <gabytrejo303@gmail.com>
2017-06-14 17:08:32 +0000

    Acciones agregadas

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ var api = {
 var cargarPagina = function(){
     cargarTareas();
     $("#add-form").submit(agregarTarea);
+    $(document).on('click', '.eliminar', eliminaRecurso);
+    $(document).on('click', '.obtenerInfo', infoRecurso);
 };
 
 var $taskList = $("#tasks-list");
@@ -15,9 +17,17 @@ var cargarTareas = function(){
     });
 };
 
+var plantillaBotones = '<button  class="glyphicon glyphicon glyphicon-zoom-in obtenerInfo" aria-hidden="true" data-id="__id__"></button>' + '<button class="glyphicon glyphicon glyphicon-pencil" aria-hidden="true"></button>' +
+  '<button class="glyphicon glyphicon glyphicon-trash eliminar" aria-hidden="true" data-id="__id__"></button>';
+
+var plantilla = "";
+
 var crearTarea = function(tarea){
     var nombre = tarea.name;
     var estado = tarea.status[0];
+   
+    plantilla = plantillaBotones.replace("__id__", tarea._id)
+    .replace("__id__", tarea._id);
     //Creacion de fila
     var $tr = $("<tr />");
     //Celda de nombre
@@ -26,9 +36,10 @@ var crearTarea = function(tarea){
     //Celda de estado
     var $estadoTd= $("<td />");
     $estadoTd.text(estado);
-
-    $tr.append($nombreTd).append($estadoTd);
+    var $eliminar = $("<td />").html(plantilla);
+    $tr.append($nombreTd).append($estadoTd).append($eliminar);
     $taskList.append($tr);
+    
 };
 
 var agregarTarea = function(e) {
@@ -40,6 +51,23 @@ var agregarTarea = function(e) {
         crearTarea(tarea);
         $("#myModal").modal("hide");
         
+    });
+};
+
+var eliminaRecurso = function(){        
+    jQuery.ajax({
+        url: api.url + $(this).data('id'),
+        type: 'DELETE',
+        success: function(msj) {
+            cargarTareas();
+        }
+    });    
+};
+
+var infoRecurso = function(){
+    $.getJSON(api.url + $(this).data('id'),function(tarea){ 
+        var nombre = tarea.name;
+        var estado = tarea.status[0];
     });
 };
 
